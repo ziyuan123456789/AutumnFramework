9f446875578c3a7ebb4cf006cb9d60955791ad72
ziyuan <3139196862@qq.com>
2024-08-16 14:54:28 +0800

    [功能完善] 优化了sql执行相关的问题

--- src/main/java/org/example/Config/BeanTestConfig.java
+++ src/main/java/org/example/Config/BeanTestConfig.java
@@ -8,6 +8,13 @@ import org.example.FrameworkUtils.AutumnCore.Annotation.MyConfig;
 import org.example.FrameworkUtils.AutumnCore.Annotation.MyPostConstruct;
 import org.example.mapper.UserMapper;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 /**
  * @author ziyuan
  * @since 2024.04
@@ -21,13 +28,27 @@ public class BeanTestConfig {
     UserMapper userMapper;
 
     @MyPostConstruct
-    public void init(){
-        try{
-            log.warn(userMapper.getAllUser(1).toString());
-        }catch (Exception e){
-            log.warn("sql执行失败了,但没关系,继续运行");
-        }
+    public void init() {
+        log.warn("现在我们来测试orm是否正常,但你很可能没有修改配置文件导致一段时间的阻塞,于是我限制了代码运行时间,如果三秒钟还没有结束那就是配置文件存在问题,我们就跳过这个环节继续运行");
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<?> future = executor.submit(() -> {
+            try {
+                log.warn(userMapper.getAllUser(1).toString());
+            } catch (Exception e) {
+                log.error("SQL 执行失败了, 但没关系, 继续运行");
+            }
+        });
 
+        try {
+            future.get(3, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            log.error("方法执行时间超过5秒，抛出异常");
+            future.cancel(true);
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("执行过程中发生异常", e);
+        } finally {
+            executor.shutdown();
+        }
     }
     @AutumnBean("BYD")
     public Car giveMeBydCar() throws Exception {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,6 @@ import org.example.FrameworkUtils.AutumnCore.Annotation.EnableAutoConfiguration;
 import org.example.FrameworkUtils.AutumnCore.Annotation.MyConfig;
 import org.example.FrameworkUtils.WebFrameworkBaseUtils.MyServers.AutumnFrameworkRunner;
 
-import java.util.HashSet;
-import java.util.Set;
-
 
 /*
   @author ziyuan
@@ -22,46 +19,5 @@ public class Main {
       autumnFrameworkRunner.run(Main.class);
     }
 
-
-    int maxDiameter = 0;
-
-    public int diameterOfBinaryTree(TreeNode root) {
-        maxDepth(root);
-        return maxDiameter;
-    }
-
-    int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int leftMax = maxDepth(root.left);
-        int rightMax = maxDepth(root.right);
-        // 后序位置，顺便计算最大直径
-        int myDiameter = leftMax + rightMax;
-        maxDiameter = Math.max(maxDiameter, myDiameter);
-
-        return 1 + Math.max(leftMax, rightMax);
-    }
-
-
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
 }
 
